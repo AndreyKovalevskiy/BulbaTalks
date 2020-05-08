f11058a88ebcc29abb40fc9b5521b5e82959f1f3
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-05-08 15:46:28 +0300

    Add protocols; rename files

---
+++ BulbaTalks/BulbaTalks/Extensions/JSONDecoder+TwitterResponseDecoding.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+protocol TwitterResponseDecoding {
+    func decodeTwitterResponse<T>(_ type: T.Type,
+                                  from data: Data) throws -> T where T: Decodable
+}
+
+extension JSONDecoder: TwitterResponseDecoding {
+    func decodeTwitterResponse<T>(_ type: T.Type,
+                                  from data: Data) throws -> T where T: Decodable {
+        let oldDateDecodingStrategy = self.dateDecodingStrategy
+        defer { self.dateDecodingStrategy = oldDateDecodingStrategy }
+        
+        do {
+            self.dateDecodingStrategy = .formatted(.twitter)
+            return try self.decode(type, from: data)
+        } catch {
+            throw error
+        }
+    }
+}
