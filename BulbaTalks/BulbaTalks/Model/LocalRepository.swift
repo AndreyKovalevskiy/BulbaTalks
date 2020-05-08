f001c932a01a069747f4ccf9c0cdcf3a48511ccd
BongDiDong <pogosskiy1999@mail.ru>
2020-05-08 15:59:08 +0300

    Create a class LocalRepository

---
+++ BulbaTalks/BulbaTalks/Model/LocalRepository.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class LocalRepository {
+    func getHomeTimeline() -> [Tweet]? {
+        let filenameWithJSON = "home_timeline"
+        guard let data = FileReader.readLocalJSONFile(from: filenameWithJSON) else { return nil }
+
+        do {
+            let arrayOfTweets = try JSONDecoder().decodeTwitterResponse([Tweet].self, from: data)
+            return arrayOfTweets
+        } catch {
+            return nil
+        }
+    }
+}
