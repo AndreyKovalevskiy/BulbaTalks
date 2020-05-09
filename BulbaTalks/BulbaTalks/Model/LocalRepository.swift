d7668895f62e74c8c58e2c565a2cbdf5d11f062e
BongDiDong <pogosskiy1999@mail.ru>
2020-05-09 15:02:33 +0300

    Update error handling

--- BulbaTalks/BulbaTalks/Model/LocalRepository.swift
+++ BulbaTalks/BulbaTalks/Model/LocalRepository.swift
@@ -5,11 +5,6 @@ class LocalRepository: Repository {
         let filenameWithJSON = "home_timeline"
         guard let data = FileReader.readLocalJSONFile(from: filenameWithJSON) else { return nil }
 
-        do {
-            let arrayOfTweets = try JSONDecoder().decodeTwitterResponse([Tweet].self, from: data)
-            return arrayOfTweets
-        } catch {
-            return nil
-        }
+        return try? JSONDecoder().decodeTwitterResponse([Tweet].self, from: data)
     }
 }
