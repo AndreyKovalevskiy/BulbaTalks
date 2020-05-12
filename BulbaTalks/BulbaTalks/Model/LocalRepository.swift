6be9d5566e0f10a6fc2e04be4249c888e7023d31
BongDiDong <pogosskiy1999@mail.ru>
2020-05-12 15:55:46 +0300

    Change json hardcode path to more flexible approach to get the path

--- BulbaTalks/BulbaTalks/Model/LocalRepository.swift
+++ BulbaTalks/BulbaTalks/Model/LocalRepository.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class LocalRepository: Repository {
     func getHomeTimeline() -> [Tweet] {
-        let jsonURL = URL(fileURLWithPath: "MockedTwitterResponses/1.1/Statuses/home_timeline.json")
+        let jsonURL = URL(fileURLWithPath: Operation.getHomeTimeline.path)
         guard let data = FileReader.read(at: jsonURL) else { return [] }
 
         return (try? JSONDecoder().decodeTwitterResponse([Tweet].self, from: data)) ?? []
