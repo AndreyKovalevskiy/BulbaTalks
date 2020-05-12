36fa8221ed7cc7192e71b5e78e201275f2720eaf
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-05-12 19:03:02 +0300

    Add a Authorization structure

---
+++ BulbaTalks/BulbaTalks/Model/Authorization.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Authorization {
+    static var isSignedIn: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: "isSignedIn")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "isSignedIn")
+        }
+    }
+}
