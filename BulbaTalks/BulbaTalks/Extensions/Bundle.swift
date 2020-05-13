13401fb96cb20e0b7d94cb6cf6c50da7a0cdef80
Dzmitry <skysilvaby@gmail.com>
2020-05-13 16:33:36 +0300

    Create Bundle extension

---
+++ BulbaTalks/BulbaTalks/Extensions/Bundle.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension Bundle {
+    func contentsOfFile(at resourceURL: URL) -> Data? {
+        guard let url = self.url(forResource: resourceURL.lastPathComponent,
+                                     withExtension: nil) else { return nil }
+        return try? Data(contentsOf: url)
+    }
+}
