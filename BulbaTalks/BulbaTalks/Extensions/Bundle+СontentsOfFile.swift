f8525f478d20fdf112c55886329efff5e983018d
Dzmitry <skysilvaby@gmail.com>
2020-05-13 20:16:50 +0300

    Remane file

--- BulbaTalks/BulbaTalks/Extensions/Bundle+СontentsOfFile.swift
+++ BulbaTalks/BulbaTalks/Extensions/Bundle+СontentsOfFile.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Bundle {
     func contentsOfFile(at resourceURL: URL) -> Data? {
         guard let url = self.url(forResource: resourceURL.lastPathComponent,
-                                     withExtension: nil) else { return nil }
+                                 withExtension: nil) else { return nil }
         return try? Data(contentsOf: url)
     }
 }
