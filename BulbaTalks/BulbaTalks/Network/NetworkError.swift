3dde3c95cc6945fc8d4e210ebbc6191f51b54d80
Hannabolotova <hannah.bolotova@gmail.com>
2020-05-15 19:46:35 +0300

    Add enum NetworkError

---
+++ BulbaTalks/BulbaTalks/Network/NetworkError.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum NetworkError: Error {
+    case error(statusCode: Int)
+    case serverError(message: String)
+    
+    var description: String {
+        switch self {
+        case .error(let statusCode):
+            return "The call failed with HTTP code \(statusCode)."
+        case .serverError(let message):
+            return "The server responded with message \(message)"
+        }
+    }
+}
