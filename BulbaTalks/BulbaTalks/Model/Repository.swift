bff9079f01f92c8cc23de4fe3a86bfa0b4971b8a
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-05-17 15:01:22 +0300

    Add the documentation comment and fix the getHomeTimeline func

--- BulbaTalks/BulbaTalks/Model/Repository.swift
+++ BulbaTalks/BulbaTalks/Model/Repository.swift
@@ -1,3 +1,8 @@
 protocol Repository {
-    func getHomeTimeline(since firstTweet: Tweet?, before lastTweet: Tweet?, completion: ([Tweet]) -> ())
+    /// Returns a collection of tweets and retweets posted by the authenticating user and the users he follows
+    /// - Parameters:
+    ///   - firstTweet: The tweet to get more recent tweets
+    ///   - lastTweet: The tweet to get more older tweets
+    ///   - completion: A closure that accepts an array of tweets
+    func getHomeTimeline(since firstTweet: Tweet?, until lastTweet: Tweet?, completion: ([Tweet]) -> Void)
 }
