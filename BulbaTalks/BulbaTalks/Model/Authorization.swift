881e5303ee967035cad4b1262b2905e4b1af49e5
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-05-17 22:02:46 +0300

    Add singOut func and the documentation comments

--- BulbaTalks/BulbaTalks/Model/Authorization.swift
+++ BulbaTalks/BulbaTalks/Model/Authorization.swift
@@ -1,6 +1,9 @@
 import Foundation
 
+/// A structure that coordinates a group of tasks related to authorization on Twitter.
 struct Authorization {
+    
+    /// A boolean value indicating whether the user is authorized.
     static private(set) var isSignedIn: Bool {
         get {
             return UserDefaults.standard.bool(forKey: "isSignedIn")
@@ -10,8 +13,21 @@ struct Authorization {
         }
     }
     
-    static func makeAuth() -> Bool {
+    /**
+     Performs user authorization process.
+     - Returns: `True` if the user was authorized, otherwise `false`.
+     */
+    static func authorize() -> Bool {
         isSignedIn = true
+        return true
+    }
+    
+    /**
+     Performs user deauthorization process.
+     - Returns: `True` if the user was deauthorized, otherwise `false`.
+     */
+    static func singOut() -> Bool {
+        isSignedIn = false
         return true
     }
 }
