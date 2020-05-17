8486419f8eca9f0307596edee54eef319fee5a9b
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-05-17 15:08:14 +0300

    Fix the documentation comment

--- BulbaTalks/BulbaTalks/Model/Repository.swift
+++ BulbaTalks/BulbaTalks/Model/Repository.swift
@@ -1,8 +1,8 @@
 protocol Repository {
-    /// Returns a collection of tweets and retweets posted by the authenticating user and the users he follows
+    /// Returns a collection of tweets and retweets posted by the authenticating user and the users he follows.
     /// - Parameters:
-    ///   - firstTweet: The tweet to get more recent tweets
-    ///   - lastTweet: The tweet to get more older tweets
-    ///   - completion: A closure that accepts an array of tweets
+    ///   - firstTweet: The tweet to get more recent tweets.
+    ///   - lastTweet: The tweet to get more older tweets.
+    ///   - completion: A closure that accepts an array of received tweets.
     func getHomeTimeline(since firstTweet: Tweet?, until lastTweet: Tweet?, completion: ([Tweet]) -> Void)
 }
