b74286944c8f6f856317762c5ff1a07af5226528
Dzmitry <skysilvaby@gmail.com>
2020-05-18 02:09:41 +0300

    Add doc comments

--- BulbaTalks/BulbaTalks/Extensions/Bundle+СontentsOfFile.swift
+++ BulbaTalks/BulbaTalks/Extensions/Bundle+СontentsOfFile.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 extension Bundle {
+    /**
+     Returns the contents of the resource file using URL.
+     - Parameter fileURL: The path to the resource file.
+     - Returns: Contents of the resource file or
+     `nil` if the file could not be located.
+     */
     func contentsOfFile(at fileURL: URL) -> Data? {
         guard let url = url(forResource: fileURL.lastPathComponent,
                                  withExtension: nil) else {
