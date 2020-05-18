e4fbfd9428c7a7256587ae2826f7d466293eac17
BongDiDong <pogosskiy1999@mail.ru>
2020-05-18 06:37:19 +0300

    Update implementation

---
+++ BulbaTalks/BulbaTalks/Model/MockRepository.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/**
+ The repository that responsible for working with mock data from Twitter APIs.
+ It uses prepared files stored in the project and works without access to the internet.
+ */
+class MockRepository {
+    func getHomeTimeline(since firstTweet: Tweet?, until lastTweet: Tweet?, completion: ([Tweet]) -> Void) {
+        let path = APIEndpoints.getHomeTimeLine(with: [:], queryParameters: [:], bodyParameters: [:]).path
+        let jsonURL = URL(fileURLWithPath: path)
+        if let data =  Bundle.main.contentsOfFile(at: jsonURL) {
+            completion(try? JSONDecoder().decodeTwitterResponse([Tweet].self, from: data) ?? [])
+            return
+        }
+        completion([])
+    }
+}
