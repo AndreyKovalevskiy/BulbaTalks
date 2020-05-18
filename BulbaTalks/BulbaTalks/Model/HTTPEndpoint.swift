c9f3afb3de3e27261fec93b02c0e59e2fa282fc5
Dzmitry <skysilvaby@gmail.com>
2020-05-18 14:17:21 +0300

    Add doc comments

--- BulbaTalks/BulbaTalks/Model/HTTPEndpoint.swift
+++ BulbaTalks/BulbaTalks/Model/HTTPEndpoint.swift
@@ -1,7 +1,13 @@
+/// The structure contains all the necessary properties for generating an HTTP request.
 struct HTTPEndpoint: HTTPRequestable {
+    /// Enum of request methods such as `GET` and `POST`.
     var method: HTTPMethodType
+    /// The path used to create the URL resource.
     var path: String
+    /// These parameters will be included in the request header. Typealias [String: String].
     var headerParamaters: HTTPHeaderParameters
+    /// These parameters will be included in the query string. Typealias [String: String].
     var queryParameters: HTTPQueryParameters
+    /// These parameters will be included in the request body. Typealias [String: Any].
     var bodyParamaters: HTTPBodyParameters
 }
