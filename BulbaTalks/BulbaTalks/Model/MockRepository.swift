b09c07cce0de5c6030a74fd3a023690b270e2f20
BongDiDong <pogosskiy1999@mail.ru>
2020-05-18 11:15:40 +0300

    Add protocol 'Repository'

--- BulbaTalks/BulbaTalks/Model/MockRepository.swift
+++ BulbaTalks/BulbaTalks/Model/MockRepository.swift
@@ -4,7 +4,7 @@ import Foundation
  The repository that responsible for working with mock data from Twitter APIs.
  It uses prepared files stored in the project and works without access to the internet.
  */
-class MockRepository {
+class MockRepository: Repository {
     func getHomeTimeline(since firstTweet: Tweet?, until lastTweet: Tweet?, completion: ([Tweet]) -> Void) {
         let path = APIEndpoints.getHomeTimeLine(with: [:], queryParameters: [:], bodyParameters: [:]).path
         let jsonURL = URL(fileURLWithPath: path)
