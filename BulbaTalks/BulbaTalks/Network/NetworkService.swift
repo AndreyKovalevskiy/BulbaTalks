42a51b14619a42c42bfde9028e89bf055301159f
Hannabolotova <hannah.bolotova@gmail.com>
2020-05-18 18:46:51 +0300

    Fix warnings and remarks. Add doc comments

--- BulbaTalks/BulbaTalks/Network/NetworkService.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkService.swift
@@ -1,21 +1,24 @@
 import Foundation
-
+/**
+ This singletone class  making networking calls.
+ */
 class NetworkService {
-    
-    private let generalConfiguration: GeneralDataSourceConfiguration
-    
-    init(generalConfiguration: GeneralDataSourceConfiguration){
-        self.generalConfiguration = generalConfiguration
-    }
-    
-    private func networkRequest(request: URLRequest, completion: @escaping CompletionHandler) -> URLSessionTask {
-    
-        let networkTask = URLSession.shared.dataTask(with: request)  { (data, response, requestError) in
-            
-            if let requestError = requestError, data != nil else {
-                completion(.failure(.error(statusCode: statusCode)))
+    typealias CompletionHandler = (Result<Data?, Error>) -> Void
+/**
+     This func reates a task that retrieves the contents of the specified URL, then calls a handler upon completion.
+     - Parameter element: url, completion handler.
+     - Returns: The new session data task.
+*/
+    private func networkRequest(request: URLRequest,
+                                completion: @escaping CompletionHandler) -> URLSessionTask {
+        let networkTask = URLSession.shared.dataTask(with: request) { (data, response, requestError) in
+            if let requestError = requestError, data != nil {
+                completion(.failure(requestError))
             }
-            
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                   return
+            }
             completion(.success(data))
         }
         networkTask.resume()
