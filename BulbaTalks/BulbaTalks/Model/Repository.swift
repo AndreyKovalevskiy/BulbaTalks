c4524fd17c7c66aa6f431dd056722efcca6579c7
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-05-19 12:31:21 +0300

    Add getAuthenticatedUser func

--- BulbaTalks/BulbaTalks/Model/Repository.swift
+++ BulbaTalks/BulbaTalks/Model/Repository.swift
@@ -1,4 +1,4 @@
-/// A type that describes methods for retrieving data provided by the Twitter API.
+/// A protocol that describes methods for retrieving data provided by the Twitter API.
 protocol Repository {
     /**
      Returns a collection of tweets and retweets posted by the authenticating user and the users he follows.
@@ -8,4 +8,11 @@ protocol Repository {
        - completion: The completion handler to call when the receipt of tweets is completed. This completion handler takes an array of tweets as a parameter.
      */
     func getHomeTimeline(since firstTweet: Tweet?, until lastTweet: Tweet?, completion: ([Tweet]) -> Void)
+    
+    /**
+     Returns authenticated user.
+     - Parameter completion: The completion handler to call when the receipt of the user is completed. This completion handler accepts the user as a parameter
+     if the user was successfully received, otherwise `nil`.
+     */
+    func getAuthenticatedUser(completion: (User?) -> Void)
 }
