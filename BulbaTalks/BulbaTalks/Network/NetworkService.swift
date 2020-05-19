e3714cfa96663e79e4ea3523a7b7d3a8e6de7471
Hannabolotova <hannah.bolotova@gmail.com>
2020-05-19 20:03:13 +0300

    Fix remarks

--- BulbaTalks/BulbaTalks/Network/NetworkService.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkService.swift
@@ -6,7 +6,7 @@ class NetworkService {
     typealias CompletionHandler = (Result<Data?, Error>) -> Void
 /**
      The function creates a task that retrieves the contents of the specified URL, then calls a handler upon completion.
-     - Parameter elements: URL, Completion Handler.
+     - Parameter elements: URLRequest, Completion Handler.
      - Returns: The new session data task.
 */
     private func networkRequest(request: URLRequest,
