91a47e1ed33d018ff2848081a19f509e42c90dc3
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-05-20 11:16:38 +0300

    Change doc comments

--- BulbaTalks/BulbaTalks/Model/Repository.swift
+++ BulbaTalks/BulbaTalks/Model/Repository.swift
@@ -1,18 +1,35 @@
-/// A protocol that describes methods for retrieving data provided by the Twitter API.
+/**
+ A protocol that describes methods for both receiving data provided by the Twitter API and for receiving mock data
+ of the corresponding Twitter API.
+ */
 protocol Repository {
     /**
      Returns a collection of tweets and retweets posted by the authenticated user and the users he is following.
      - Parameters:
-       - firstTweet: The tweet object. If the parameter is not nil, tweets with an identifier greater than the identifier of this tweet will be received.
-       - lastTweet: The tweet object. If the parameter is not nil, tweets with an identifier less than the identifier of this tweet will be received.
-       - completion: The completion handler to call when the receipt of tweets is completed. This completion handler takes an array of tweets as a parameter.
+       - firstTweet: The tweet object. If the parameter is not `nil`, then only tweets that are more recent than
+       the firstTweet will be received, otherwise, the parameter will not affect the receipt of tweets.
+       - lastTweet: The tweet object. If the parameter is not `nil`, then only tweets that are more later than
+       the lastTweet will be received, otherwise, the parameter will not affect the receipt of tweets.
+       - completion: The completion handler to call when the request for receiving of tweets is complete.
+     
+         This completion handler takes the following parameter:
+
+         **[Tweet]**
+
+         The array of the received tweets if the request completed successfully and the tweets were received,
+         otherwise, an empty array.
      */
     func getHomeTimeline(since firstTweet: Tweet?, until lastTweet: Tweet?, completion: ([Tweet]) -> Void)
     
     /**
      Returns authenticated user.
-     - Parameter completion: The completion handler to call when the receipt of the user is completed. This completion handler accepts the user as a parameter
-     if the user was successfully received, otherwise `nil`.
+     - Parameter completion: The completion handler to call when the request for receiving the user is complete.
+     
+       This completion handler takes the following parameter:
+     
+       **User**
+     
+       A user if the request completed successfully and the user was received, otherwise, `nil`.
      */
     func getAuthenticatedUser(completion: (User?) -> Void)
 }
