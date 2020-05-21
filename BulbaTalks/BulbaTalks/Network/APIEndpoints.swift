aa5b08dcf65241b345cda07c5c573d85254c5559
Dzmitry <skysilvaby@gmail.com>
2020-05-21 11:51:53 +0300

    Update doc comments

--- BulbaTalks/BulbaTalks/Network/APIEndpoints.swift
+++ BulbaTalks/BulbaTalks/Network/APIEndpoints.swift
@@ -1,14 +1,17 @@
-/// The object that contains methods for generating requests.
+/// The object that contains methods to make requests.
 struct APIEndpoints {
     /**
-     Returns the object contains all the necessary properties
-     for generating an HTTP request to get home_timeline.
+     Returns the object contains properties to make
+     an HTTP request to get home_timeline.
      - Parameters:
-        - headersParameters: These parameters will be included in the request header.
-        - queryParameters: These parameters will be included in the query string.
-        - bodyParameters: These parameters will be included in the request body.
-     - Returns: The object contains all the necessary properties
-     for generating an HTTP request to get home_timeline.
+        - headersParameters: These parameters will be included in
+        the request header.
+        - queryParameters: These parameters will be included in
+        the query string.
+        - bodyParameters: These parameters will be included in
+        the request body.
+     - Returns: The object contains properties to make
+     an HTTP request to get home_timeline.
      */
     static func getHomeTimeline(with headersParameters: HTTPHeaderParameters,
                                 queryParameters: HTTPQueryParameters,
