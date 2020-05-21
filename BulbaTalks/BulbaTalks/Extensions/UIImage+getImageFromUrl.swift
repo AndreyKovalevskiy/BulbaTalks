a5b5c089eb613bcfa62b09c54a38f1e120f7fd4e
Belka <badpanda@tut.by>
2020-05-21 13:09:17 +0300

    Changed extension

--- BulbaTalks/BulbaTalks/Extensions/UIImage+getImageFromUrl.swift
+++ BulbaTalks/BulbaTalks/Extensions/UIImage+getImageFromUrl.swift
@@ -2,13 +2,12 @@ import UIKit
 
 extension UIImage {
     static func getImage(fromUrl url: String, completion: @escaping (UIImage?) -> Void) {
-        var image: UIImage?
         if let url = URL(string: url) {
-            do {
-                let data = try Data(contentsOf: url, options: [])
-                image = UIImage(data: data)
-            } catch {}
+            if let data = try? Data(contentsOf: url) {
+                let image = UIImage(data: data)
+                completion(image)
+            }
         }
-        completion(image)
+        completion(nil)        
     }
 }
