edcb4030a7fb1fbae9b6dac6b89884f3170bdec0
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-05-21 11:38:03 +0300

    Fix doc comments

--- BulbaTalks/BulbaTalks/Model/Repository.swift
+++ BulbaTalks/BulbaTalks/Model/Repository.swift
@@ -1,35 +1,33 @@
 /**
- A protocol that describes methods for both receiving data provided by the Twitter API and for receiving mock data
- of the corresponding Twitter API.
+ A protocol that describes methods for both receiving data provided
+ by the Twitter API and for receiving mock data of the corresponding
+ Twitter API.
  */
 protocol Repository {
     /**
-     Returns a collection of tweets and retweets posted by the authenticated user and the users he is following.
+     Returns a collection of tweets and retweets posted by
+     the authenticated user and the users he is following.
      - Parameters:
-       - firstTweet: The tweet object. If the parameter is not `nil`, then only tweets that are more recent than
-       the firstTweet will be received, otherwise, the parameter will not affect the receipt of tweets.
-       - lastTweet: The tweet object. If the parameter is not `nil`, then only tweets that are more later than
-       the lastTweet will be received, otherwise, the parameter will not affect the receipt of tweets.
-       - completion: The completion handler to call when the request for receiving of tweets is complete.
-     
-         This completion handler takes the following parameter:
-
-         **[Tweet]**
-
-         The array of the received tweets if the request completed successfully and the tweets were received,
-         otherwise, an empty array.
+       - firstTweet: The `Tweet` object. If the parameter
+       is not `nil`, then only tweets that are more recent than
+       the firstTweet will be received, otherwise, the parameter
+       will not affect the receipt of tweets.
+       - lastTweet: The `Tweet` object. If the parameter
+       is not `nil`, then only tweets that are earlier than
+       the lastTweet will be received, otherwise, the parameter
+       will not affect the receipt of tweets.
+       - completion: A closure that takes an array of
+       `Tweet` objects as a parameter if receiving the tweets
+       is successful; otherwise, an empty array.
      */
     func getHomeTimeline(since firstTweet: Tweet?, until lastTweet: Tweet?, completion: ([Tweet]) -> Void)
-    
+
     /**
      Returns authenticated user.
-     - Parameter completion: The completion handler to call when the request for receiving the user is complete.
-     
-       This completion handler takes the following parameter:
-     
-       **User**
-     
-       A user if the request completed successfully and the user was received, otherwise, `nil`.
+     - Parameter completion: A closure that takes
+     `User` object corresponding to the current authenticated
+     user as a parameter, or `nil` if the authenticated user
+     was not received.
      */
     func getAuthenticatedUser(completion: (User?) -> Void)
 }
