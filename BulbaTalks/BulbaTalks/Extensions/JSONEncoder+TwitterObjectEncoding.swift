75edf54d45164c11342af0ad1512b4ef6145983e
Dzmitry <skysilvaby@gmail.com>
2020-05-25 20:18:55 +0300

    Update files

--- BulbaTalks/BulbaTalks/Extensions/JSONEncoder+TwitterObjectEncoding.swift
+++ BulbaTalks/BulbaTalks/Extensions/JSONEncoder+TwitterObjectEncoding.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 protocol TwitterObjectEncoding {
-    func encodeTwitterObject<T>(_ value: T) throws -> Data where T : Encodable
+    func encodeTwitterObject<T>(_ value: T) throws -> Data where T: Encodable
 }
 
 extension JSONEncoder: TwitterObjectEncoding {
-    func encodeTwitterObject<T>(_ value: T) throws -> Data where T : Encodable {
-        let oldDateDecodingStrategy = self.dateEncodingStrategy
+    func encodeTwitterObject<T>(_ value: T) throws -> Data where T: Encodable {
+        let oldDateDecodingStrategy = dateEncodingStrategy
         defer { self.dateEncodingStrategy = oldDateDecodingStrategy }
-        
+
         do {
             dateEncodingStrategy = .formatted(.twitter)
             return try encode(value)
