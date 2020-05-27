5adb0462d5f6f025c628990ec0824ef17d73bad2
Dzmitry <skysilvaby@gmail.com>
2020-05-28 00:46:10 +0300

    Update implementation

--- BulbaTalks/BulbaTalks/Network/APIEndpoints.swift
+++ BulbaTalks/BulbaTalks/Network/APIEndpoints.swift
@@ -1,24 +1,47 @@
 /// The API Endpoints.
 struct APIEndpoints {
     /**
-     Returns the object contains properties to make
-     an HTTP request to get home_timeline.
+     Returns the object contains parameters to construct
+     `HTTPEndpoint` to get `home_timeline`.
+
      - Parameters:
-        - headersParameters: These parameters will be included in
+        - headerParameters: These parameters will be included in
         the request header.
         - queryParameters: These parameters will be included in
         the query string.
         - bodyParameters: These parameters will be included in
         the request body.
-     - Returns: The object contains properties to make
-     an HTTP request to get home_timeline.
+     - Returns: The `HTTPEndpoint` object.
      */
-    static func getHomeTimeline(with headersParameters: HTTPHeaderParameters,
+    static func getHomeTimeline(with headerParameters: HTTPHeaderParameters,
                                 queryParameters: HTTPQueryParameters,
                                 bodyParameters: HTTPBodyParameters) -> HTTPRequestable {
         return HTTPEndpoint(method: .get,
                             path: "1.1/statuses/home_timeline.json",
-                            headerParamaters: headersParameters,
+                            headerParamaters: headerParameters,
+                            queryParameters: queryParameters,
+                            bodyParamaters: bodyParameters)
+    }
+
+    /**
+     Returns the object contains parameters to construct
+     `HTTPEndpoint` to get `verify_credentials`.
+
+     - Parameters:
+        - headerParameters: These parameters will be included in
+        the request header.
+        - queryParameters: These parameters will be included in
+        the query string.
+        - bodyParameters: These parameters will be included in
+        the request body.
+     - Returns: The `HTTPEndpoint` object.
+     */
+    static func getAuthenticatedUser(with headerParameters: HTTPHeaderParameters,
+                                     queryParameters: HTTPQueryParameters,
+                                     bodyParameters: HTTPBodyParameters) -> HTTPRequestable {
+        return HTTPEndpoint(method: .get,
+                            path: "1.1/account/verify_credentials.json",
+                            headerParamaters: headerParameters,
                             queryParameters: queryParameters,
                             bodyParamaters: bodyParameters)
     }
