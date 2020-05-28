eeacc4f580b8efedd456cda2badcc1362fb3c9f2
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-05-28 11:32:30 +0300

    Delete empty line

--- BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
@@ -4,7 +4,7 @@ import Foundation
 A protocol contains properties required to create a valid `URLRequest`.
 */
 protocol NetworkConfiguration {
-    ///A value that identifies the location of a resource for  receiving data.
+    ///A value that identifies the location of a resource for receiving data.
     var baseURL: URL { get }
     ///HTTP Authorization headers.
     var commonHeaders: HTTPHeaders { get }
