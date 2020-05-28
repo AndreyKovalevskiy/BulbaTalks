430ec2770b1f7f17796c87a0b71e87851fcafc56
Hannabolotova <hannah.bolotova@gmail.com>
2020-05-28 14:22:57 +0300

    Fix remarks. Add doc comments

--- BulbaTalks/BulbaTalks/Network/HTTPNetworkingProtocol.swift
+++ BulbaTalks/BulbaTalks/Network/HTTPNetworkingProtocol.swift
@@ -1,7 +1,23 @@
 import Foundation
-
+/**
+ Describes an actual implomentation of those requirements
+ for `NetworkServise` class.
+ */
 protocol HTTPNetworking {
-    typealias CompletionHandler = (Result<Data?, NetworkError>) -> Void
-    
-    func httpRequest(apiEndpoint: HTTPRequestable, completion: @escaping CompletionHandler) -> URLSessionTask?
+    /**
+     Provides a new name for an existing data type which describes value
+     that represents either a ` success` or a `failure`:
+     - `Success` with the value of the result.
+     - `Failure` with the type that implements Error protocol.
+     */
+    typealias CompletionHandler = (Result<Data, Error>) -> Void
+    /**
+     Makes request based on the specified  `URLRequest` object, and calls
+     a handler upon completion.
+     - Parameters:
+        - apiEndpoint: A valid `URLRequest`.
+        - completion: The ` CompletionHandler` to call when the load
+     request is complete.
+     */
+    func httpRequest(apiEndpoint: HTTPRequestable, completion: @escaping CompletionHandler)
 }
