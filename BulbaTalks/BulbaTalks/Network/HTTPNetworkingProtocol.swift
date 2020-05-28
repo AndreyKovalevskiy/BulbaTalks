271dd60c071f2aa585e7b42a8c7168617aa20103
Hannabolotova <hannah.bolotova@gmail.com>
2020-05-28 14:32:00 +0300

    Update doc commets

--- BulbaTalks/BulbaTalks/Network/HTTPNetworkingProtocol.swift
+++ BulbaTalks/BulbaTalks/Network/HTTPNetworkingProtocol.swift
@@ -5,19 +5,21 @@ import Foundation
  */
 protocol HTTPNetworking {
     /**
-     Provides a new name for an existing data type which describes value
-     that represents either a ` success` or a `failure`:
+     Provides a new name for an existing data type which
+     describes value that represents either a ` success`
+     or a `failure`:
      - `Success` with the value of the result.
-     - `Failure` with the type that implements Error protocol.
+     - `Failure` with the type that implements
+     Error protocol.
      */
     typealias CompletionHandler = (Result<Data, Error>) -> Void
     /**
-     Makes request based on the specified  `URLRequest` object, and calls
-     a handler upon completion.
+     Makes request based on the specified  `URLRequest`
+     object, and calls a handler upon completion.
      - Parameters:
         - apiEndpoint: A valid `URLRequest`.
-        - completion: The ` CompletionHandler` to call when the load
-     request is complete.
+        - completion: The ` CompletionHandler`
+     to call when the load request is complete.
      */
     func httpRequest(apiEndpoint: HTTPRequestable, completion: @escaping CompletionHandler)
 }
