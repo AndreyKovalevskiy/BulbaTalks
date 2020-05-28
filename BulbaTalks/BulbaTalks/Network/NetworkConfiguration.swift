219523fac0e6c068802cef97c202a9c7f4e2c7e7
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-05-28 11:46:13 +0300

    Change doc comments for NetworkConfiguration

--- BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
@@ -6,6 +6,6 @@ A protocol contains properties required to create a valid `URLRequest`.
 protocol NetworkConfiguration {
     ///A value that identifies the location of a resource for receiving data.
     var baseURL: URL { get }
-    ///HTTP Authorization headers.
+    ///A value that contains HTTP Authorization headers.
     var commonHeaders: HTTPHeaders { get }
 }
