900958072df565c3ddd9543afe3baa2f75989aa4
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-05-28 22:35:39 +0300

    Add quotes

--- BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
@@ -7,6 +7,6 @@ protocol NetworkConfiguration {
     /// The primary part of the URL.
     /// After it follows a variable part of the URL, defined by `HTTPEndpoint`.
     var baseURL: URL { get }
-    /// Headers that are used in all URLRequest.
+    /// Headers that are used in all `URLRequest`.
     var commonHeaders: HTTPHeaders { get }
 }
