c38997bbc2361c3900b55f08099f6011cab1331d
Hannabolotova <hannah.bolotova@gmail.com>
2020-05-28 18:01:43 +0300

    Fix doc comments

--- BulbaTalks/BulbaTalks/Network/NetworkService.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkService.swift
@@ -1,23 +1,25 @@
 import Foundation
 /**
- An object that makes network calls.
+ The class that makes network calls.
  */
 class NetworkService {
     typealias CompletionHandler = (Result<Data, Error>) -> Void
-/**
-     Creates a task that retrieves the contents of the specified URL,
-     then calls a handler upon completion.
+    /**
+     Creates a task that retrieves the contents of the
+    `URLRequest`, then calls a handler upon completion.
      - Parameters:
-        - request: The URLRequest to be retrived.
-        - completion: The completion handler to call when the load request
-     is complete. This completion handler takes the following parameters:
-            - data: The data returned by the server, or nil if the request was fail.
-            - response:  An object that provides HTTP headers and status code.
-            - error:  An error object that indicates why the request failed,
-            or nil if the request was successful.
-     
+        - request: The `URLRequest` to be retrived.
+        - completion: The completion handler to call
+     when the load request is complete.
+     This completion handler takes the following parameters:
+            - data: The data returned by the server,
+     or nil if the request was failed.
+            - response: An object represents a response
+     to a request and provides response headers.
+            - error: An error object that indicates why
+     the request failed, or nil if the request was successful.
      - Returns: The new session data task.
-*/
+     */
     private func networkRequest(request: URLRequest,
                                 completion: @escaping CompletionHandler) -> URLSessionTask {
         let networkTask = URLSession.shared.dataTask(with: request) { (data, response, requestError) in
