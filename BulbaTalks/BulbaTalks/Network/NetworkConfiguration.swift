8e1896c34d34ee5e8b38d1d78a83215fa3520c5c
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-05-29 09:47:24 +0300

    Change the structure of doc comments for baseURL

--- BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkConfiguration.swift
@@ -4,8 +4,10 @@ import Foundation
  Contains the necessary properties to create a valid `URLRequest`.
  */
 protocol NetworkConfiguration {
-    /// The primary part of the URL.
-    /// After it follows a variable part of the URL, defined by `HTTPEndpoint`.
+    /**
+     The primary part of the URL.
+     After it follows a variable part of the URL, defined by `HTTPEndpoint`.
+     */
     var baseURL: URL { get }
     /// Headers that are used in all `URLRequest`.
     var commonHeaders: HTTPHeaders { get }
