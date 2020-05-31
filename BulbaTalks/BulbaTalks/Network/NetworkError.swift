07eb4d886dc6435383ffbba9df3d767f9be1133b
Hannabolotova <hannah.bolotova@gmail.com>
2020-05-31 16:55:09 +0300

    Remove case

--- BulbaTalks/BulbaTalks/Network/NetworkError.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkError.swift
@@ -2,15 +2,11 @@
 enum NetworkError: Error {
     /// Describes a network error with a specific status code.
     case error(statusCode: Int)
-    /// Describes a network error with a custom description.
-    case otherError(message: String)
     
     var description: String {
         switch self {
         case .error(let statusCode):
             return "The call failed with HTTP code \(statusCode)."
-        case .otherError(let message):
-            return "The call failed with message \(message)"
         }
     }
 }
