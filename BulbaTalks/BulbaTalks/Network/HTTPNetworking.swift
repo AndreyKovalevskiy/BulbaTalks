2987e97250132a080d1645150030a784b5be8bf0
Hannabolotova <hannah.bolotova@gmail.com>
2020-06-01 00:12:54 +0300

    Fix doc comments

--- BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
+++ BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
@@ -5,18 +5,19 @@ import Foundation
  */
 protocol HTTPNetworking {
     /**
-     Provides a new name for an existing data type.
+     Provides a new name for an cosure which we can call in complition.
      */
     typealias CompletionHandler = (Result<Data, NetworkError>) -> Void
     /**
-     Makes request based on the specified  `URLRequest`
-     object, and calls a handler upon completion.
+     Makes request based on the
+     `HTTPRequestable` protocol, and calls a handler
+     upon completion.
      - Parameters:
-        - apiEndpoint: A URL that enables the API to
+        - apiEndpoint: That enables the API to
         gain access to resources on a server.
         - completion: The `CompletionHandler`
-        to call when the load request is completed.
+        to call when the request is completed.
      - Returns: URLSessionTask.
      */
-    func httpRequest(apiEndpoint: HTTPRequestable, completion: @escaping CompletionHandler) -> URLSessionTask
+    func httpRequest(apiEndpoint: HTTPRequestable, completion: @escaping CompletionHandler) -> URLSessionTask?
 }
