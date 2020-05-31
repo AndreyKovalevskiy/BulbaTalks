6dd79be93d2c7342e18fcba785879d09559ad6aa
Belka <badpanda@tut.by>
2020-05-31 18:07:36 +0300

    Changed doc comments

--- BulbaTalks/BulbaTalks/Extensions/UIViewController+StoryboardID.swift
+++ BulbaTalks/BulbaTalks/Extensions/UIViewController+StoryboardID.swift
@@ -2,7 +2,7 @@ import UIKit
 
 extension UIViewController {
     /**
-     Gets the view controller identifier which uses in
+     Gets the view controller identifier which is uses in
      the storyboard configuration at the interface builder.
      */
     static var storyboardID: String {
