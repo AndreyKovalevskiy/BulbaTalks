05eb7363361efe5115f95395f6b51274b7e7bec0
Hannabolotova <hannah.bolotova@gmail.com>
2020-06-01 18:11:42 +0300

    Fix doc comments for NetworkService and typealias

--- BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
+++ BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
@@ -1,20 +1,18 @@
 import Foundation
 /**
  Responsible for data communications and
- provides interaction with the server.
+ provides interaction with network.
  */
 protocol HTTPNetworking {
     /**
-     Provides a new name for the enum Result type.
+     Defines an alternative name for enum  Result type
+     so as not to repeat the code.
      */
     typealias CompletionHandler = (Result<Data, NetworkError>) -> Void
     /**
-     Makes request based on the
-     `HTTPRequestable` protocol and calls a handler
-     upon completion.
+     Makes async request to the given HTTP endpoint.
      - Parameters:
-        - apiEndpoint: Point through which we
-        interact with the server.
+        - apiEndpoint: API HTTP endpoint.
         - completion: The `CompletionHandler`
         to call when the request is completed.
      - Returns: URLSessionTask.
