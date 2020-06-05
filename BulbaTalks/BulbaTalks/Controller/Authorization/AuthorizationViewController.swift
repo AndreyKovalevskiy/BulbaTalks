5e22f22c402730e2c57befbc3048ffa883f5f241
Belka <badpanda@tut.by>
2020-06-05 13:08:16 +0300

    Implement behavior of login button touch

--- BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
+++ BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
@@ -1,5 +1,15 @@
 import UIKit
 
 class AuthorizationViewController: UIViewController {
-    @IBAction func redirectToTwitterLoginPage(_: Any) {}
+    @IBAction func redirectToTwitterLoginPage(_ sender: UIButton) {
+        UIButton.animate(withDuration: 0.2,
+                         animations: {
+                             sender.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                         },
+                         completion: { _ in
+                             UIButton.animate(withDuration: 0.2, animations: {
+                                 sender.transform = CGAffineTransform.identity
+        })
+        })
+    }
 }
