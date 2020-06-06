633e9758bfcf090f4850819ac7d768dde58d7ef6
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-06-06 10:30:57 +0300

    Changed Mark; Add func

--- BulbaTalks/BulbaTalks/Controller/ListOfTweets/ListOfTweetsViewController.swift
+++ BulbaTalks/BulbaTalks/Controller/ListOfTweets/ListOfTweetsViewController.swift
@@ -1,24 +1,27 @@
 import UIKit
 
 class ListOfTweetsViewController: UIViewController {
-    // MARK: Properties
+    // MARK: IBOutlet
 
     @IBOutlet var tableView: UITableView!
     @IBOutlet var navigationBar: UINavigationBar!
     @IBOutlet var tabBar: UITabBar!
 
-    /// Contains the height of tab bar.
+    /// Constants related to the tab bar.
     private enum TabBar {
         static let heightInLandscapeOrientation: CGFloat = 50
     }
 
-    /**
-     Contains the size for the custom `UIButton` and
-     for the anchor’s size attribute.
-     */
-    private enum ConstantSize {
-        static let width: CGFloat = 30
-        static let height: CGFloat = 30
+    /// Constants related to the custom `UIButton`.
+    private enum CustomButton {
+        static let barButtonItemWidth: CGFloat = 30
+        static let barButtonItemHeight: CGFloat = 30
+    }
+
+    /// Constants related to the layout anchor of `UIView`.
+    private enum LayoutAnchor {
+        static let barButtonItemViewWidthAnchor: CGFloat = 30
+        static let barButtonItemViewHeightAnchor: CGFloat = 30
     }
 
     override func viewDidLoad() {
@@ -34,36 +37,63 @@ class ListOfTweetsViewController: UIViewController {
         }
     }
 
-    // MARK: Private func
+    // MARK: Private functions
 
+    /// Uses to customize `UIViewController`.
     private func configureViewController() {
-        tableView.registerCell(of: ListOfTweetsTableViewCell.self)
-        tabBar.barTintColor = .white
+        configureTableView()
+        configureTabBar()
+        configureNavigationBar()
+    }
 
+    /// Uses to customize `UITableView`.
+    private func configureTableView() {
+        tableView.registerCell(of: ListOfTweetsTableViewCell.self)
+    }
+
+    /// Uses to customize `UITabBar`.
+    private func configureTabBar() {
+        tabBar.barTintColor = .white
+    }
+
+    /// Uses to customize `UINavigationBar`.
+    private func configureNavigationBar() {
+        configureTitleViewOfNavigationBar()
+        configureLeftBarButtonItem()
+    }
+
+    /// Adds `UIImage` in the title of navigation bar.
+    private func configureTitleViewOfNavigationBar() {
         if let logoOfTwitter = UIImage(named: "logoOfTwitter") {
             let titleViewWithImage = UIImageView(image: logoOfTwitter)
             titleViewWithImage.contentMode = .scaleAspectFit
             navigationItem.titleView = titleViewWithImage
         }
+    }
 
+    /**
+     Adds a rounded left `UIBarButtonItem` with the image
+     of the authenticated user.
+     */
+    private func configureLeftBarButtonItem() {
         let imageFromUserProfile = UIImage()
         let barButtonItem = UIButton(frame: CGRect(x: 0, y: 0,
-                                                   width: ConstantSize.width,
-                                                   height: ConstantSize.height))
+                                                   width: CustomButton.barButtonItemWidth,
+                                                   height: CustomButton.barButtonItemHeight))
         barButtonItem.layer.masksToBounds = true
         barButtonItem.layer.cornerRadius = barButtonItem.frame.height / 2
         barButtonItem.setImage(imageFromUserProfile, for: .normal)
         barButtonItem.widthAnchor.constraint(equalToConstant:
-            ConstantSize.width).isActive = true
+            LayoutAnchor.barButtonItemViewWidthAnchor).isActive = true
         barButtonItem.heightAnchor.constraint(equalToConstant:
-            ConstantSize.height).isActive = true
+            LayoutAnchor.barButtonItemViewHeightAnchor).isActive = true
         barButtonItem.addTarget(self, action: #selector(openProfile), for: .touchUpInside)
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: barButtonItem)
 
         navigationBar.items = [navigationItem]
     }
 
-    // MARK: Actions
+    // MARK: IBAction
 
     @objc func openProfile(_: UIBarButtonItem) {}
 
