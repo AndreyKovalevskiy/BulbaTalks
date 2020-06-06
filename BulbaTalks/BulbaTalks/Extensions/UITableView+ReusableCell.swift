bc73464f90ab45761a38e8391597f5751c62d3ef
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-06-06 10:28:18 +0300

    Change doc comment

--- BulbaTalks/BulbaTalks/Extensions/UITableView+ReusableCell.swift
+++ BulbaTalks/BulbaTalks/Extensions/UITableView+ReusableCell.swift
@@ -1,14 +1,14 @@
 import UIKit
 
 /**
- Provides the ability to create and dequeue the cell.
+ Provides the ability to register and dequeue the cell.
  */
 extension UITableView {
     /**
      Loads the cell from the xib resource file and
      registers it with the table view.
      The xib file should be in the main bundle.
-     - Parameter _: The class type of the cell to register.
+     - Parameter typeCell: The class type of the cell to register.
      */
     func registerCell<T: UITableViewCell>(of _: T.Type) {
         let nibFileWithCell = UINib(nibName: T.reuseIdentifier, bundle: nil)
@@ -18,7 +18,7 @@ extension UITableView {
     /**
      Dequeues an existing cell if one is available or creates
      a new one using the class or nib file you previously registered.
-     - Parameter _: The class type of the cell to dequeue.
+     - Parameter typeCell: The class type of the cell to dequeue.
      - Returns: The reusable cell or `nil` if no cell is available
      for reuse and you did not register a class or nib file.
      */
