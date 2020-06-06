445c7be11ad9be2a3968584bd1582e0681e29956
Dzmitry <skysilvaby@gmail.com>
2020-06-06 11:34:04 +0300

    Remove typealias

--- BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
+++ BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
@@ -5,11 +5,6 @@ import Foundation
  */
 protocol HTTPNetworking {
     /**
-     Introduce this in order not to repeat our code
-     and to make the code more clean and readable.
-     */
-    typealias CompletionHandler = (Result<Data, NetworkError>) -> Void
-    /**
      Makes asynchronous request
      to the given HTTP endpoint and
      returns the new session task.
@@ -19,5 +14,6 @@ protocol HTTPNetworking {
         to call when the request is completed.
      - Returns: `URLSessionTask`.
      */
-    func httpRequest(apiEndpoint: HTTPRequestable, completion: @escaping CompletionHandler) -> URLSessionTask?
+    func httpRequest(apiEndpoint: HTTPRequestable,
+                     completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionTask?
 }
