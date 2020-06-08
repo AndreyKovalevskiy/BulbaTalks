9dac185cb346fb5db7af5e76f8bd7ffdfe02e449
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-06-08 11:27:45 +0300

    Add new cases and description for them

--- BulbaTalks/BulbaTalks/Network/NetworkError.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkError.swift
@@ -1,12 +1,32 @@
 /// Describes network error.
 enum NetworkError: Error {
-    /// Describes a network error with a specific status code.
-    case error(statusCode: Int)
-    
+    case badNetworkConection
+    case clientError
+    case failedRequest
+    case invalidResponse
+    case invalidURL
+    case invalidURLRequest
+    case noData
+    case serverError
+
     var description: String {
         switch self {
-        case .error(let statusCode):
-            return "The call failed with HTTP code \(statusCode)."
+        case .badNetworkConection:
+            return "Bad network conection."
+        case .clientError:
+            return "A client-side error occurred while executing the request."
+        case .failedRequest:
+            return "Network request failed."
+        case .invalidResponse:
+            return "Invalid response received."
+        case .invalidURL:
+            return "The connection failed due to a malformed URL."
+        case .invalidURLRequest:
+            return "The connection failed due to a malformed URLRequest."
+        case .noData:
+            return "Response returned with no data."
+        case .serverError:
+            return "The server failed to fulfill a request."
         }
     }
 }
