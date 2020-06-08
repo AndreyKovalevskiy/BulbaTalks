487979a55801e5eeba5a39b32c9f0431438a7b80
Dzmitry <skysilvaby@gmail.com>
2020-06-09 00:06:39 +0300

    Update doc comments for protocol

--- BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
+++ BulbaTalks/BulbaTalks/Network/HTTPNetworking.swift
@@ -1,17 +1,16 @@
 import Foundation
 /**
- Defines a method for interacting with a network using
- HTTP requests.
+ Defines methods and properties for interacting with a network
+ using HTTP requests.
  */
 protocol HTTPNetworking {
     /**
      Performs network request to receive data using the API.
      - Parameters:
         - apiEndpoint: API HTTP endpoint.
-        - completion: The completion handler that accepts
-        `Result` as a parameter, where the success case
-        will store a received data and the failure case will be
-        some `NetworkError`.
+        - completion: The completion handler where the success
+     case will get some data returned by the server and
+     the failure case will get some `NetworkError`.
      - Returns: Instance of `URLSessionTask`.
      */
     func httpRequest(apiEndpoint: HTTPRequestable,
