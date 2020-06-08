e5f5fffdccb8739d1b550f5c0c95537bc8804383
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-06-08 19:13:21 +0300

    Add doc comments for cases

--- BulbaTalks/BulbaTalks/Network/NetworkError.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkError.swift
@@ -1,14 +1,43 @@
-/// Describes network error.
+/**
+ Describes network error.
+ */
 enum NetworkError: Error {
+    /**
+     The error related to bad network connection.
+     */
     case badNetworkConection
+    /**
+     A client-side error while executing the request.
+     */
     case clientError
+    /**
+     Network request failure.
+     */
     case failedRequest
+    /**
+     The error related to the invalid response received.
+     */
     case invalidResponse
+    /**
+     The error related to a malformed URL.
+     */
     case invalidURL
+    /**
+     The error related to a malformed URLRequest.
+     */
     case invalidURLRequest
+    /**
+     The error related to the response returned with no data.
+     */
     case noData
+    /**
+     The server failed to fulfill a request.
+     */
     case serverError
 
+    /**
+     A more detailed description of the network error.
+     */
     var description: String {
         switch self {
         case .badNetworkConection:
