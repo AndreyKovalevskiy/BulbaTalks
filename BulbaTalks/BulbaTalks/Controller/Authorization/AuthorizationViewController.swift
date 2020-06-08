fef58aac4203150ad784dfaa79455e5630e3ca61
Belka <badpanda@tut.by>
2020-06-08 18:57:06 +0300

    Changed code style and removed a magic numbers

--- BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
+++ BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
@@ -2,14 +2,16 @@ import UIKit
 
 class AuthorizationViewController: UIViewController {
     @IBAction func redirectToTwitterLoginPage(_ sender: UIButton) {
-        UIButton.animate(withDuration: 0.2,
+        let animationDuration = 0.2
+        let scaleTransformation: CGFloat = 0.9
+        UIButton.animate(withDuration: animationDuration,
                          animations: {
-                             sender.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                             sender.transform = CGAffineTransform(scaleX: scaleTransformation, y: scaleTransformation)
                          },
                          completion: { _ in
-                             UIButton.animate(withDuration: 0.2, animations: {
+                             UIButton.animate(withDuration: animationDuration, animations: {
                                  sender.transform = CGAffineTransform.identity
-        })
-        })
+                             })
+                         })
     }
 }
