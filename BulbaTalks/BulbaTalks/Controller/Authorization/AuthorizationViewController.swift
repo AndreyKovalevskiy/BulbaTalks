f72ad3ba8c24a321b56e782c90b662d7a9c0b39e
Belka <badpanda@tut.by>
2020-06-09 18:33:37 +0300

    Changed doc comments

--- BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
+++ BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
@@ -1,18 +1,22 @@
 import UIKit
 
 class AuthorizationViewController: UIViewController {
-    /// Parameters for the log in button animation
+    /**
+     Parameters for the log in button animation.
+     */
     private enum ButtonAnimationParameter {
         static let animationDuration = 0.2
         static let scaleTransformation: CGFloat = 0.9
     }
 
     @IBAction func redirectToTwitterLoginPage(_ sender: UIButton) {
-        /// The log in button touch animation
+        /**
+         The log in button touch animation.
+         */
         UIButton.animate(withDuration: ButtonAnimationParameter.animationDuration,
                          animations: {
                              sender.transform = CGAffineTransform(scaleX: ButtonAnimationParameter.scaleTransformation,
-                                                                  y: ButtonAnimationParameter.scaleTransformation)s
+                                                                  y: ButtonAnimationParameter.scaleTransformation)
                          },
                          completion: { _ in
                              UIButton.animate(withDuration: ButtonAnimationParameter.animationDuration, animations: {
