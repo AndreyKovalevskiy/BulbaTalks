ff7c3388e26eadb212219a3fa6b0a4635853f71e
Tatsiana Gouteeva <gouteeva@gmail.com>
2020-06-11 11:38:42 +0300

    Changed doc comment of rounded image view

--- BulbaTalks/BulbaTalks/View/ReusableControls/RoundedImageView.swift
+++ BulbaTalks/BulbaTalks/View/ReusableControls/RoundedImageView.swift
@@ -2,18 +2,13 @@ import UIKit
 
 /**
  Contains properties of the class `UIImageView` that
- will render in Interface Builder in real-time.
- These properties will be set in the Attributes inspector,
- modifing them allows see the image view getting updated
- in real-time within the storyboard.
- To get these properties needs to set in Identity Inspector
- the `UIImageView` to `RoundedImageView` class.
+ can be rendered in the Interface Builder.
  */
 @IBDesignable
 class RoundedImageView: UIImageView {
     /**
      Defines the radius to use when drawing
-     rounded corners for the layer’s background.
+     rounded corners of `UIImageView`.
      */
     @IBInspectable var cornerRadius: CGFloat = 0.0 {
         didSet {
