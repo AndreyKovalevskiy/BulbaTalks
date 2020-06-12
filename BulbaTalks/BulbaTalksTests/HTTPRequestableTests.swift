c37e0539c9eed082ea89f069e765537cd494d064
BongDiDong <pogosskiy1999@mail.ru>
2020-06-12 10:56:06 +0300

    Add unit tests for the 'HTTPRequestable' protocol

---
+++ BulbaTalks/BulbaTalksTests/HTTPRequestableTests.swift
@@ -0,0 +1,33 @@
+//
+//  HTTPRequestableTests.swift
+//  BulbaTalksTests
+//
+//  Created by Андрей Погосский on 6/11/20.
+//  Copyright © 2020 CocoaHeadsBY. All rights reserved.
+//
+
+import XCTest
+
+class HTTPRequestableTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
