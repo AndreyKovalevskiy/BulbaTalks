c8a3dbddd9ef62845a43922f8875072702da2d63
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-06-13 21:15:11 +0300

    Fix one of cases and change doc comments

--- BulbaTalks/BulbaTalks/Network/NetworkError.swift
+++ BulbaTalks/BulbaTalks/Network/NetworkError.swift
@@ -3,30 +3,33 @@
  */
 enum NetworkError: Error {
     /**
-     The response with a specific HTTP status code that is
-     outside the 200...299 range.
+     HTTP response with the non-successful status
+     code is received (outside the 2xx codes).
+     The status codes described in the RFC7231.
      */
     case badResponse(statusCode: Int)
     /**
-     The request was not completed due to the reason
+     The request can't be completed due to the reason
      specified in the `description`.
      */
     case failedRequest(description: String)
     /**
-     The response has an unexpected type that failed to cast
-     to HTTPURLResponse.
+     The response does not conform to HTTP protocol or
+     it is corrupted.
      */
-    case invalidResponse
+    case nonHTTPResponse
     /**
-     Failed to get the URL of the request.
+     The URL from the request can't be used to make
+     a network call.
      */
     case invalidURL
     /**
-     Failed to get the `URLRequest`.
+     The HTTP request can't be used to make a network call.
      */
     case invalidURLRequest
     /**
-     No data is in the received response.
+     No data is in the received response or the data is
+     malformed.
      */
     case noData
 }
