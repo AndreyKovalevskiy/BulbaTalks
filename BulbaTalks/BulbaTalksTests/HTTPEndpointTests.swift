1d82d1c36e70b2037c67e18b5a1ea8b37493a1a2
Dzmitry <skysilvaby@gmail.com>
2020-06-13 12:02:27 +0300

    Update unit tests

--- BulbaTalks/BulbaTalksTests/HTTPEndpointTests.swift
+++ BulbaTalks/BulbaTalksTests/HTTPEndpointTests.swift
@@ -2,34 +2,12 @@
 import XCTest
 
 class HTTPEndpointTests: XCTestCase {
-    func testMethodPathHeaderParametersQueryParametersPropertiesContainExpectedParametersWhenAreInitializedWithGivenParameters() {
+    func testPublicPropertiesContainExpectedValuesWhenEndpointIsInitializedWithGivenParameters() {
         // Given
         let expectedMethod = HTTPMethodType.get
         let expectedPath = "1.1/statuses/home_timeline.json"
         let expectedHeaderParameters = [String: String]()
         let expectedQueryParameters = [String: String]()
-
-        // When
-        let timelineEndpoint = HTTPEndpoint(method: .get,
-                                            path: "1.1/statuses/home_timeline.json",
-                                            headerParameters: [:],
-                                            queryParameters: [:],
-                                            bodyParameters: [:])
-
-        // Then
-        let timelineEnpointMethod = timelineEndpoint.method
-        let timelineEnpointPath = timelineEndpoint.path
-        let timelineEnpointHeaderParameters = timelineEndpoint.headerParameters
-        let timelineEnpointQueryParameters = timelineEndpoint.queryParameters
-
-        XCTAssertEqual(timelineEnpointMethod, expectedMethod)
-        XCTAssertEqual(timelineEnpointPath, expectedPath)
-        XCTAssertEqual(timelineEnpointHeaderParameters, expectedHeaderParameters)
-        XCTAssertEqual(timelineEnpointQueryParameters, expectedQueryParameters)
-    }
-
-    func testBodyParametersPropertyContainsExpectedParameterWhenIsInitializedWithGivenParameter() {
-        // Given
         let initialBodyParameters = ["body": "Body"]
         let expectedBodyParameters = initialBodyParameters
         let expectedParameterValue = expectedBodyParameters["body"]
@@ -42,9 +20,17 @@ class HTTPEndpointTests: XCTestCase {
                                             bodyParameters: initialBodyParameters)
 
         // Then
+        let timelineEnpointMethod = timelineEndpoint.method
+        let timelineEnpointPath = timelineEndpoint.path
+        let timelineEnpointHeaderParameters = timelineEndpoint.headerParameters
+        let timelineEnpointQueryParameters = timelineEndpoint.queryParameters
         let timelineEnpointBodyParameters = timelineEndpoint.bodyParameters
         let value = timelineEnpointBodyParameters["body"] as? String
 
+        XCTAssertEqual(timelineEnpointMethod, expectedMethod)
+        XCTAssertEqual(timelineEnpointPath, expectedPath)
+        XCTAssertEqual(timelineEnpointHeaderParameters, expectedHeaderParameters)
+        XCTAssertEqual(timelineEnpointQueryParameters, expectedQueryParameters)
         XCTAssertNotNil(value)
         XCTAssertEqual(value, expectedParameterValue)
     }
