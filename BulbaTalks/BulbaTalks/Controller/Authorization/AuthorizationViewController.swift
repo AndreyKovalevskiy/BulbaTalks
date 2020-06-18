8d3c676b9f970a17fbf802e9d4708ee4d8d0183d
Belka <badpanda@tut.by>
2020-06-18 18:09:57 +0300

    Added comment inside redirectToTwitterLoginPage function

--- BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
+++ BulbaTalks/BulbaTalks/Controller/Authorization/AuthorizationViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class AuthorizationViewController: UIViewController {
     @IBAction func redirectToTwitterLoginPage(_: Any) {
+        // We use the stub of the signIn function.
+        // If signIn is successful, the ListOfTweets
+        // screen is presented.
         Authentication.signIn { isSignedIn in
             if isSignedIn, let viewController = AppStoryboard.listOfTweets.viewController(of:
                 ListOfTweetsViewController.self) {
